cf0a302b96840e66704416c87f55b23c65f3353d
TheComputationalCore <dineshchandra962@gmail.com>
2025-11-24 04:47:04 +0530

    Update chart.py

--- budget_app/chart.py
+++ budget_app/chart.py
@@ -3,9 +3,7 @@ def create_spend_chart(categories):
     total_spent = 0
 
     for category in categories:
-        spent = sum(
-            -item["amount"] for item in category.ledger if item["amount"] < 0
-        )
+        spent = sum(-item["amount"] for item in category.ledger if item["amount"] < 0)
         spent_by_category.append(spent)
         total_spent += spent
 
@@ -17,18 +15,15 @@ def create_spend_chart(categories):
             percentage = 0
         percentages.append(int(percentage))
 
-    chart = "Percentage spent by category
-"
+    chart = "Percentage spent by category\n"
 
     for i in range(100, -10, -10):
         line = f"{str(i).rjust(3)}| "
         for percentage in percentages:
             line += "o  " if percentage >= i else "   "
-        chart += line + "
-"
+        chart += line + "\n"
 
-    chart += "    " + "-" * (3 * len(categories) + 1) + "
-"
+    chart += "    " + "-" * (3 * len(categories) + 1) + "\n"
 
     max_name_length = max(len(category.name) for category in categories)
     for i in range(max_name_length):
@@ -36,8 +31,6 @@ def create_spend_chart(categories):
         for category in categories:
             char = category.name[i] if i < len(category.name) else " "
             line += f"{char}  "
-        chart += line + "
-"
+        chart += line + "\n"
 
-    return chart.rstrip("
-")
+    return chart.rstrip("\n")
